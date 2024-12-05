f55d22a55fabf9978b3112e763257b1f765363f0
orsusrilatha <srilatha222000@gmail.com>
2024-12-05 12:05:36 +0530

    Update settings.py

--- TeluguWebMedia/settings.py
+++ TeluguWebMedia/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'embed_video',
+    'django-ckeditor',
 ]
 
 
@@ -71,7 +72,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware',django-ckeditor
+
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
